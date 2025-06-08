ce19168689b6a3a7b2d9fa66d87f261d88de139e
tamilarasi <contact.tamil2@gmail.com>
2025-06-08 22:08:23 +0530

    added the pythicode

---
+++ datatypes.py
@@ -0,0 +1,10 @@
+# Data types;
+
+x=5            #int type:
+print(type(x))
+x="5"         #str type:
+print(type(x))
+x=5.0          #float type:
+print(type(x))
+x=5+0j         #complex type:
+print(type(x))
---
+++ operators/artimetic.py
@@ -0,0 +1,28 @@
+# Artimetic Operators:
+
+x=5
+y=3
+print(x+y)  # Addition operators
+x=5
+y=3
+print(x-y)  # subraction operators
+x=5
+y=3
+print(x*y)  # Multiplication operators
+x=5
+y=3
+print(x/y)  # Division operators
+x=5
+y=3
+print(x%y)  # Modulation operators
+x=5
+y=3
+print(x**y)  # Exponentiation operators
+x=5
+y=3
+print(x//y)  # Floor division operators
+
+
+
+# Assignment Operators:
+
