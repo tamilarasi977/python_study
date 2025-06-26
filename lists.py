6afe456c815f53d733b663a0ed17027bd4d8de21
tamilarasi <contact.tamil2@gmail.com>
2025-06-26 08:54:14 +0530

    added some chages

--- lists.py
+++ lists.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Lists:
 
 mylist=["apple","banana","orange"]
@@ -13,51 +12,38 @@ thislist = ["apple", "banana", "cherry"]
 print(thislist[2])
 
 # Returns banana to kiwi
-
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-
 print(thislist[1:5])
 
 # Returns from kiwi to end
-
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-
 print(thislist[4:])
 
 # Returns from orange  to melon end 
-
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-
 print(thislist[-4:-1])
 
-
-
 # Add the number `4` at the 6th index of the list `l`.  
-
 l=[1,22,3,44,55,6,11,1200]
 l.insert(6,4)
 print(l)
 
 #Remove the value `44` from the list `l`.  
-
 l=[1,22,3,44,55,6,11,1200]
 l.remove(44)
 print(l)
 
 # Append the number `88` to the end of the list `l`.  
-
 l=[1,22,3,44,55,6,11,1200]
 l.append(88)
 print(l)
 
 #Remove the value at the 2nd index of the list `l`.
- 
 l=[1,22,3,44,55,6,11,1200]
 l.pop(2)
 print(l)
 
 # Merge the list `m = [1, 2, 3, 546]` into the list `l`.
-
 l=[1,22,3,44,55,6,11,1200]
 m = [1, 2, 3, 546]
 l.extend(m)
@@ -67,73 +53,3 @@ print(l)
 
 
 
-=======
-# Lists:
-
-mylist=["apple","banana","orange"]
-print(mylist)
-
-# length;
-mylist=["apple","banana","orange"]
-print(len(mylist))
-
-#index;
-thislist = ["apple", "banana", "cherry"]
-print(thislist[2])
-
-# Returns banana to kiwi
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-
-print(thislist[1:5])
-
-# Returns from kiwi to end
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-
-print(thislist[4:])
-
-# Returns from orange  to melon end 
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-
-print(thislist[-4:-1])
-
-
-
-# Add the number `4` at the 6th index of the list `l`.  
-
-l=[1,22,3,44,55,6,11,1200]
-l.insert(6,4)
-print(l)
-
-#Remove the value `44` from the list `l`.  
-
-l=[1,22,3,44,55,6,11,1200]
-l.remove(44)
-print(l)
-
-# Append the number `88` to the end of the list `l`.  
-
-l=[1,22,3,44,55,6,11,1200]
-l.append(88)
-print(l)
-
-#Remove the value at the 2nd index of the list `l`.
- 
-l=[1,22,3,44,55,6,11,1200]
-l.pop(2)
-print(l)
-
-# Merge the list `m = [1, 2, 3, 546]` into the list `l`.
-
-l=[1,22,3,44,55,6,11,1200]
-m = [1, 2, 3, 546]
-l.extend(m)
-print(l)
-
- 
-
-
-
->>>>>>> 9e1cbf0959faf3839296b454d0f2d63d5957b558
