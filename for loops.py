698986a2221f08c96adb94d96f7738b9daea52e1
tamilarasi <contact.tamil2@gmail.com>
2025-06-30 17:21:22 +0530

    loops added

---
+++ for loops.py
@@ -0,0 +1,53 @@
+
+# String using for loop:
+
+x="hello world"
+for i in x:
+    print(i)
+
+
+# EX 2:    
+
+fruits ={"apple","banana","chery"}
+
+for x in fruits:
+    print(fruits)
+
+# Ex 3:
+
+list=[10,20,30,40]
+sum=0
+
+for i in list:
+    sum=sum+i
+
+print("total value:", sum)   # return 100  
+
+# for loop with range:
+
+for x in range(1,12):
+    print(x)  
+
+# If statement:
+
+for i in range(1,21):
+    if (i%2==0):
+        print(i)  
+
+# Break statement:
+
+for i in ["banana","apple","orange"]:
+    if i == "apple":
+     print(i)
+    else:
+        print("end") 
+
+
+
+
+
+
+ 
+
+
+    
